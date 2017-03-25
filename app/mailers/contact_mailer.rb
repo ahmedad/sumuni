2878bfe659f6a16cfec5a74266fd9832adca8eee
ahmedad <ahmedad2077@gmail.com>
2017-03-25 14:35:24 +0300

    changed email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-	default to: 'shamsudin35@gmail.com'
+	default to: 'info@sumunitech.co.ke'
 	def contact_email(name, email, request, massage)
 		@name = name
 		@email = email
